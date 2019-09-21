e7c4bedd8be26edc10242a812e6cfd84cf09c7ea
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-09-21 18:29:04 +0100

    more useContext added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 const BookList = () => {
+  const { books } = useContext(BookContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
   return (
@@ -10,9 +12,13 @@ const BookList = () => {
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
