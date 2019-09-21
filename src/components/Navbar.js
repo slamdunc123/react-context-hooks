6a5109244fd3210ec73d36e69bdc80eff6573923
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-09-21 12:24:48 +0100

    basic template built

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    return (
+      <nav>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
