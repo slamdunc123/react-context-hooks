73f3fdf13d9d343050a4b2a5c45fc2e6a140046d
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-09-21 14:48:13 +0100

    consumer approach

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  static contextType = ThemeContext; // declare variable to consume the specific context
+  // static contextType = ThemeContext; // declare variable to consume the specific context
   render() {
     // console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+
+    // using the consumer approach is allowed in functional components too
+    // you can also use multiple contexts within one component
+    // static approach only works in class components and only one context is allowed
+
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
